4ccbcc89343da157126713d65151037afdd4e720
FatherOfGloom <antonuh2002@gmail.com>
2025-02-01 02:41:54 +0300

    PPM header skipped by passing it in a slice range + removed comments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use core::time;
-use std::io::{BufReader, Result, Write};
+use std::io::{BufReader, Result};
 use std::process::{Child, ChildStdout};
 
 use std::thread;
@@ -11,6 +11,8 @@ use std::{
 
 use terminal_size::{terminal_size, Height, Width};
 
+static CHARS_LIGHT: &'static [u8] = b"  .:!+*e$@8";
+
 struct CLIRenderer {
     w: usize,
     h: usize,
@@ -21,34 +23,30 @@ impl CLIRenderer {
         CLIRenderer { w: w, h: h }
     }
 
-    // TODO: pass a skipped header slice
+    fn setup_console(&self) {
+        print!("{}", r"\e[?25l");
+        print!("{}", "\u{001b}[2J");
+    }
+
     fn render_ppm(&self, buf: &[u8]) {
         let w = self.w;
         let h = self.h;
 
         let mut v: Vec<u8> = Vec::new();
 
-        // print!("{}", "\x1B[2J");
-        // print!("{}", "\x1b[1;1H");
-
-        let ppm_header_size = 9 + w.to_string().len() + h.to_string().len();
-
         for y in 0..h {
             for x in (0..(w * 3)).step_by(3) {
-                let idx: usize = (y * (w * 3) + x + ppm_header_size).into();
+                let idx: usize = (y * (w * 3) + x).into();
                 let r = buf[idx];
                 let g = buf[idx + 1];
                 let b = buf[idx + 2];
-                let a = pixel_to_ascii(r, g, b);
-                v.push(a);
+                let ascii = pixel_to_ascii(r, g, b);
+                v.push(ascii);
             }
-
-            // v.push(b'\n');
         }
 
         print!("{}", "\u{001b}[H");
         print!("\r{}", core::str::from_utf8(v.as_slice()).unwrap());
-        std::io::stdout().flush().unwrap();
     }
 }
 
@@ -57,6 +55,7 @@ struct FfmpegReader {
     buf_reader: Option<BufReader<ChildStdout>>,
     frame_buffer: Vec<u8>,
     frame_size: usize,
+    frame_header_size: usize,
 }
 
 impl FfmpegReader {
@@ -65,17 +64,8 @@ impl FfmpegReader {
 
         // using raw_arg since arg() function passes args to ffmpeg with quotation marks on windows
         // https://github.com/rust-lang/rust/issues/92939
-        // cmd.raw_arg(&format!("-i {}", video_file_path))
-        //     .raw_arg(&format!("-s {}x{}", w, h))
-        //     .raw_arg("-vframes 1")
-        //     // .raw_arg("-pix_fmt rgb 24")
-        //     .raw_arg("-vcodec ppm")
-        //     .raw_arg("-f image2pipe")
-        //     .raw_arg("-");
-        // ffmpeg -i - -ss 00:00:3 -s 650x390 -vframes 1 -c:v png -f image2pipe -
         cmd.raw_arg(&format!("-i {}", video_file_path))
             .raw_arg(&format!("-s {}x{}", w, h))
-            // .raw_arg("-f image2pipe -")
             .raw_arg("-f image2pipe")
             .raw_arg("-pix_fmt rgb24")
             .raw_arg("-vcodec ppm")
@@ -94,15 +84,18 @@ impl FfmpegReader {
             buf_reader: Some(BufReader::new(stdout)),
             frame_buffer: vec![],
             frame_size: frame_buffer_alloc_size,
+            frame_header_size: ppm_header_size,
         })
     }
 
     fn get_frame_buffer_ppm(&mut self) -> Option<&[u8]> {
-        let m = "Cannot get a frame buffer with uninitialized BufReader.";
+        let b = &mut self.frame_buffer;
 
-        if self.frame_buffer.len() > 0 {
-            self.frame_buffer.clear();
+        if b.len() > 0 {
+            b.clear();
         }
+
+        let m = "Cannot get a frame buffer with uninitialized BufReader.";
 
         let bytes_read = self
             .buf_reader
@@ -110,14 +103,14 @@ impl FfmpegReader {
             .expect(m)
             .by_ref()
             .take(self.frame_size as u64)
-            .read_to_end(&mut self.frame_buffer)
+            .read_to_end(b)
             .unwrap(); 
 
         if bytes_read == 0 {
             return None;
         }
 
-        Some(self.frame_buffer.as_slice())
+        Some(&b[self.frame_header_size..])
     }
 
     fn wait_for_child(&mut self) -> Result<()> {
@@ -130,10 +123,6 @@ impl FfmpegReader {
         Ok(())
     }
 }
-
-static CHARS_LIGHT: &'static [u8] = b"  .:!+*e$@8";
-// static CHARS_LIGHT: &'static [u8] = b"`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@";
-// static CHARS_COLOR: &'static [u8] = b".*es@";
 
 fn pixel_to_ascii(r: u8, g: u8, b: u8) -> u8 {
     let b = rgb_to_brightness(r, g, b);
@@ -154,17 +143,14 @@ fn brightness_to_ascii(b: f32) -> u8 {
 fn video_to_ascii(file_path: &str) -> Result<()> {
     let (terminal_w, terminal_h) = match terminal_size() {
         Some((Width(w), Height(h))) => (w as usize, h as usize - 1),
-        // _ => panic!("Couldn't get terminal window size"),
-        _ => (200, 150),
+        _ => panic!("Couldn't get terminal window size"),
     };
-
-    println!("console w: {} h: {}", terminal_w, terminal_h);
 
     let mut ffmpeg_reader = FfmpegReader::new(file_path, terminal_w, terminal_h)?;
     let renderer = CLIRenderer::new(terminal_w, terminal_h);
     let frame_delay = time::Duration::from_millis(20);
 
-    print!("{}", "\u{001b}[2J");
+    renderer.setup_console();
 
     loop {
         match ffmpeg_reader.get_frame_buffer_ppm() {
